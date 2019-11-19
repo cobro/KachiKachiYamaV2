196088c8cba8f083089bc1efd1e470d95197841f
casper <casper@ditdatdot.dk>
2019-11-19 15:08:14 +0100

    SmokingFire

---
+++ FireParticles/MinMaxCurve.cs
@@ -0,0 +1,14 @@
+﻿namespace FireParticles
+{
+    internal class MinMaxCurve
+    {
+        private global::System.Single v1;
+        private global::System.Single v2;
+
+        public MinMaxCurve(global::System.Single v1, global::System.Single v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
